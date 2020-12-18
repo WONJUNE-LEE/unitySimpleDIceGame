3527fb0797a05ea482726a768b823b98518d6178
이원준 <55430857+WONJUNE-LEE@users.noreply.github.com>
2020-12-18 21:55:56 +0900

    Add files via upload

---
+++ Scripts/cupMove.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cupMove : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Button shakebut;
+    public Button checkBut;
+
+    bool buttonInput = false;
+
+    float speed = 10f;
+    Vector3 moveToDice = new Vector3(-3, 12, 0);
+    Vector3 coverTheDice = new Vector3(-3, 1.9f, 0);
+    Vector3 originPos;
+    Quaternion originRot;
+
+    public GameObject dice1;
+    public GameObject dice2;
+    public GameObject dice3;
+
+
+    public static bool myturn = true; // 컴퓨터한테 턴을 넘겨줌
+    public static bool comturn = false; // 컴퓨터 턴인지 확인용
+
+    public int gameMode = 0; // 컵흔들기위해서 방향에 따라서 숫자를 주는놈
+    int where = -1; // 컵방향
+    int count = 0; // 컵 몇번흔들지 정할놈
+
+    int[] diceRot = new int[] { 0, 90, 180, 270 };
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPos = transform.position;
+        originRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameMode == 0) {
+            transform.rotation = originRot;
+            transform.position = originPos;
+        }
+        if (buttonInput) {
+            buttonInput = false;
+
+            gameMode = 1;
+        }
+
+        if (gameMode == 1) {
+            transform.position = Vector3.MoveTowards(transform.position, moveToDice, Time.deltaTime * speed);
+            if(transform.position == moveToDice) {
+                transform.Rotate(new Vector3(180, 0, 0));
+                gameMode = 2;
+            }
+        }
+        else if(gameMode == 2) {
+            Debug.Log("Now In Mode 2");
+            audioSource.Play();
+            transform.position = Vector3.MoveTowards(transform.position, coverTheDice, Time.deltaTime * speed);
+            if(transform.position == coverTheDice) {
+                gameMode = 3;
+            }
+        }
+
+        else if (gameMode == 3) {
+            dice1.SetActive(false);
+            dice2.SetActive(false);
+            dice3.SetActive(false);
+            Shake();
+        }
+        else if(gameMode == 4) {
+            transform.position = Vector3.MoveTowards(transform.position, moveToDice, Time.deltaTime * speed);
+            if(transform.position == moveToDice) {
+                gameMode = 5;
+                transform.Rotate(new Vector3(-180, 0, 0));
+            }
+        }
+        else if(gameMode == 5) {
+            transform.position = Vector3.MoveTowards(transform.position, originPos, Time.deltaTime * speed);
+            if(transform.position == originPos) {
+                gameMode = 0;
+                gameManager.turnCount--;
+                if (myturn == false) {
+                    comturn = true;
+                }
+
+                if (gameManager.turnCount > 0) {
+                    checkBut.interactable = true;
+                    shakebut.interactable = true;
+                }
+                else if(gameManager.turnCount == 0 && myturn == true){
+                    checkBut.interactable = true;
+                }
+                else {
+                    myturn = false;
+                }
+            }
+        }
+    }
+
+    void Shake() {
+        if (transform.position.x > 1.5) {
+            where = 1;
+            count++;
+        }
+        if(transform.position.x < -6.5) {
+            where = -1;
+        }
+        if(count == 3) {
+            int dice1X = diceRot[Random.Range(0, 4)];
+            int dice2X = diceRot[Random.Range(0, 4)];
+            int dice3X = diceRot[Random.Range(0, 4)];
+
+            int dice1Z = diceRot[Random.Range(0, 4)];
+            int dice2Z = diceRot[Random.Range(0, 4)];
+            int dice3Z = diceRot[Random.Range(0, 4)];
+
+            dice1.SetActive(true);
+            dice1.transform.Rotate(dice1X, dice1.transform.rotation.y, dice1Z);
+
+            dice2.SetActive(true);
+            dice2.transform.Rotate(dice2X, dice2.transform.rotation.y, dice2Z);
+
+            dice3.SetActive(true);
+            dice3.transform.Rotate(dice3X, dice3.transform.rotation.y, dice3Z);
+            transform.position = transform.position;
+
+            count = 0;
+            gameMode = 4;
+            
+        }
+        transform.Translate(Vector3.left * 10f * Time.deltaTime * where);
+    }
+
+    public void ButtonClick() {
+        buttonInput = true;
+        shakebut.interactable = false;
+        checkBut.interactable = false;
+    }
+
+}
---
+++ Scripts/gameManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    public Text dice1T;
+    public Text dice2T;
+    public Text dice3T;
+    public Text myScoreT;
+    public Text comScoreT;
+
+    public GameObject winText;
+    public GameObject loseText;
+
+    public Button shakeBut;
+    public Button checkBut;
+
+    public Text diceCount;
+
+    float scoreTime;
+    public static int turnCount = 3;
+    int myScore = 0;
+    int comScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkBut.interactable = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        diceCount.text = "가능횟수 : " + turnCount.ToString();
+        //cupMove에서 myturn을 넘겨줌
+        if(cupMove.myturn == false && cupMove.comturn == true) {
+            if (dice3T.text == dice2T.text) {
+                if (dice2T.text == dice1T.text) {
+                    comScore = 20;
+                }
+            }
+            comScore = int.Parse(dice1T.text) + int.Parse(dice2T.text) + int.Parse(dice3T.text);
+            comScoreT.text = comScore.ToString();
+
+            if(comScore > myScore) {
+                loseText.SetActive(true);
+                checkBut.interactable = false;
+                shakeBut.interactable = false;
+            }
+            else {
+                winText.SetActive(true);
+                checkBut.interactable = false;
+                shakeBut.interactable = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "SIDE1") {
+            dice1T.text = "1";
+        }
+        else if (other.gameObject.tag == "SIDE2") {
+            dice1T.text = "2";
+        }
+        else if (other.gameObject.tag == "SIDE3") {
+            dice1T.text = "3";
+        }
+        else if (other.gameObject.tag == "SIDE4") {
+            dice1T.text = "4";
+        }
+        else if (other.gameObject.tag == "SIDE5") {
+            dice1T.text = "5";
+        }
+        else if (other.gameObject.tag == "SIDE6") {
+            dice1T.text = "6";
+        }
+
+        if(other.gameObject.tag == "SIDE2-1") {
+            dice2T.text = "1";
+        }
+        else if(other.gameObject.tag == "SIDE2-2") {
+            dice2T.text = "2";
+        }
+        else if (other.gameObject.tag == "SIDE2-3") {
+            dice2T.text = "3";
+        }
+        else if (other.gameObject.tag == "SIDE2-4") {
+            dice2T.text = "4";
+        }
+        else if (other.gameObject.tag == "SIDE2-5") {
+            dice2T.text = "5";
+        }
+        else if (other.gameObject.tag == "SIDE2-6") {
+            dice2T.text = "6";
+        }
+
+        if (other.gameObject.tag == "SIDE3-1") {
+            dice3T.text = "1";
+        }
+        else if (other.gameObject.tag == "SIDE3-2") {
+            dice3T.text = "2";
+        }
+        else if (other.gameObject.tag == "SIDE3-3") {
+            dice3T.text = "3";
+        }
+        else if (other.gameObject.tag == "SIDE3-4") {
+            dice3T.text = "4";
+        }
+        else if (other.gameObject.tag == "SIDE3-5") {
+            dice3T.text = "5";
+        }
+        else if (other.gameObject.tag == "SIDE3-6") {
+            dice3T.text = "6";
+        }
+    }
+
+    public void ShakeButtonClick() {
+        
+    }
+
+    public void CheckButtonClick() {
+        turnCount = 0;
+
+        myScore = int.Parse(dice1T.text) + int.Parse(dice2T.text) + int.Parse(dice3T.text);
+        if (dice3T.text == dice2T.text) {
+            if(dice2T.text == dice1T.text) {
+                myScore = 20;
+            }
+        }
+        myScoreT.text = myScore.ToString();
+
+        checkBut.interactable = false;
+        cupMove.comturn = true;
+    }
+
+    public void RestartButton() {
+        SceneManager.LoadScene("Main");
+    }
+}
